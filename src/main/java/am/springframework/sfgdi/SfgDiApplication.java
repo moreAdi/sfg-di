b33b3b3d59f1fbe3afcd9604156891ea60d4ac3a
Aditya <moreaditya082@gmail.com>
2022-03-20 17:39:53 +0530

    profile annotation of spring

--- src/main/java/am/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/am/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package am.springframework.sfgdi;
 
-import am.springframework.sfgdi.controllers.ConstructorInjectedController;
-import am.springframework.sfgdi.controllers.MyController;
-import am.springframework.sfgdi.controllers.PropertyInjectedController;
-import am.springframework.sfgdi.controllers.SetterInjectedController;
+import am.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println("----Primary Bean----");
