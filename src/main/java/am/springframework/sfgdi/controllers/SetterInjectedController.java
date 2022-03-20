3c202dcddd127a4f5121d71735f351bfcf6a71b2
Aditya <moreaditya082@gmail.com>
2022-03-20 13:31:51 +0530

    First change

---
+++ src/main/java/am/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,16 @@
+package am.springframework.sfgdi.controllers;
+
+import am.springframework.sfgdi.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return  greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/am/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,6 @@
+package am.springframework.sfgdi.services;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
